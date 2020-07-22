784d474989c418990d459a2e965e3742d12cabd8
Vladimir Struts <vladimir.struts@yotec.net>
2020-07-22 21:20:36 +0200

    add more tests

--- src/app/services/grid-data.service.spec.ts
+++ src/app/services/grid-data.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { GridDataService } from './grid-data.service';
+import { LinkConstants } from '../constants/link-constants';
 
 describe('GridDataService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -12,4 +13,12 @@ describe('GridDataService', () => {
     const service: GridDataService = TestBed.get(GridDataService);
     expect(service).toBeTruthy();
   });
+
+  it('getData() should be called with params', () => {
+    const service: GridDataService = TestBed.get(GridDataService);
+    service.getData(LinkConstants.dataSource).subscribe(() =>
+      expect(service.getData).toHaveBeenCalledWith(LinkConstants.dataSource)
+    );
+  });
+
 });
